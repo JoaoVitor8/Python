3616f0847a78f1c3830a5fdad23f61a41f6c6f97
João Vitor Ezequiel <joaoezeki@gmail.com>
2020-09-28 20:10:32 -0300

    ...

--- Exercícios/Exercícios Net/net017.py
+++ Exercícios/Exercícios Net/net017.py
@@ -8,3 +8,18 @@
 #misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os
 # valores para cima, isto é, considere latas cheias.
 
+import math
+
+area = float(input('tamanho em metros quadrados: '))
+
+litro = area/6  #quantidade de litros
+latas18 = (area)/(108)  #quantidade de latas para pintar a área especificada
+galao = (area)/(21.6)  #quantidade de galao para pintar a área especificada
+
+if area > 0:  # a area tem que ser maior que zero caso contrario os valores de entradas estão errados.
+    preco = math.ceil(latas18)*80
+    print('A quantidade de latas de 18 litros a serem compradas é:', math.ceil(latas18), '\nPreço total é de:', preco, 'Reais')
+    preco2 = math.ceil(galao)*25
+    print('A quantidade de galao de 3.6 litros a serem compradas é', math.ceil(galao), '\nPreço total foi:', preco2)
+else:
+    print('Dados invalidos')
--- Exercícios/Exercícios Net/net018.py
+++ Exercícios/Exercícios Net/net018.py
@@ -1,3 +1,7 @@
 #Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet
 #(em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
+tamanho = float(input('Tamanho do arquivo (MB): '))
+velocidade = float(input('Velocidade de Internet (MBps): '))
+
+print(f'Tempo aproximado de download: {(tamanho / velocidade) * 60} Minutos')
